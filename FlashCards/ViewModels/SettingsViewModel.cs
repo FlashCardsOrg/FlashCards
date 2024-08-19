fe2a72606b3859a4b33f46ccb782d1ac2f22c855
DarkSoul <83767949+D4rk-S0ul@users.noreply.github.com>
2024-08-19 19:06:55 +0200

    Implemented Item Repeater and DataTemplate

--- FlashCards/ViewModels/SettingsViewModel.cs
+++ FlashCards/ViewModels/SettingsViewModel.cs
@@ -1,13 +1,11 @@
-﻿using System.Reflection;
-using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Reflection;
 
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 using FlashCards.Contracts.Services;
 using FlashCards.Helpers;
 
-using Microsoft.UI.Xaml;
 using Windows.ApplicationModel;
 
 namespace FlashCards.ViewModels;
@@ -25,12 +23,16 @@ public partial class SettingsViewModel : ObservableRecipient
     [ObservableProperty]
     private string _selectedLanguageTag;
 
+    [ObservableProperty]
+    private ObservableCollection<Box> _boxes;
+
     public SettingsViewModel(IThemeSelectorService themeSelectorService)
     {
         _themeSelectorService = themeSelectorService;
         _versionDescription = GetVersionDescription();
         _selectedTheme = _themeSelectorService.Theme.ToString();
         _selectedLanguageTag = GetSelectedLanguageTag();
+        _boxes = GetBoxes();
     }
 
     private static string GetSelectedLanguageTag()
@@ -56,4 +58,20 @@ public partial class SettingsViewModel : ObservableRecipient
         var appDisplayName = WinUI3Localizer.Localizer.Get().GetLocalizedString("AppDisplayName");
         return $"{appDisplayName} - {version.Major}.{version.Minor}.{version.Build}";
     }
+
+    private static ObservableCollection<Box> GetBoxes()
+    {
+        ObservableCollection<Box> boxes = new ObservableCollection<Box>();
+        boxes.Add(new Box("MyBox 1", 0));
+        boxes.Add(new Box("MyBox 2", 1));
+        return boxes;
+
+    }
 }
+
+public class Box(string boxName, int selectedIndex)
+{
+    public string BoxName { get; set; } = boxName;
+    public int SelectedIndex { get; set; } = selectedIndex;
+}
+
