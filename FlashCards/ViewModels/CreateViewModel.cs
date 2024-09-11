35a7219483dabfe38a1f7d7798372dff1175bb18
DarkSoul <83767949+D4rk-S0ul@users.noreply.github.com>
2024-09-11 21:43:44 +0200

    Added Subjets and tags to CreateViewModel

--- FlashCards/ViewModels/CreateViewModel.cs
+++ FlashCards/ViewModels/CreateViewModel.cs
@@ -1,10 +1,35 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FlashCards.Contracts.Services;
+using System.Collections.ObjectModel;
 
 namespace FlashCards.ViewModels;
 
 public partial class CreateViewModel : ObservableRecipient
 {
-    public CreateViewModel()
+    private readonly IDatabaseService _databaseService;
+
+    [ObservableProperty]
+    private ObservableCollection<Subject> _subjects;
+
+    [ObservableProperty]
+    private ObservableCollection<Tag> _tags;
+
+    public CreateViewModel(IDatabaseService databaseService)
     {
+        _databaseService = databaseService;
+        _subjects = GetSubjects();
+        _tags = GetTags();
+    }
+
+    private ObservableCollection<Subject> GetSubjects()
+    {
+        ObservableCollection<Subject> subjects = new(_databaseService.GetSubjects().Select(subject => new Subject(subject.Id, subject.Name)));
+        return subjects;
+    }
+
+    private ObservableCollection<Tag> GetTags()
+    {
+        ObservableCollection<Tag> tags = new(_databaseService.GetTags().Select(tag => new Tag(tag.Id, tag.Name)));
+        return tags;
     }
 }
