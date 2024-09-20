d2e52c0c82a2434ad58414535d52d1a8280a3e7f
DarkSoul <bruno.bittrich@outlook.com>
2024-09-20 21:31:02 +0200

    Created JSONFlashCard.cs

---
+++ FlashCards/JSONModels/JSONFlashCard.cs
@@ -0,0 +1,21 @@
+﻿using FlashCards.ViewModels;
+
+namespace FlashCards.JSONModels;
+
+public class JSONFlashCard
+{
+    public FlashCardSide Front { get; set; }
+    public FlashCardSide Back { get; set; }
+
+    public JSONFlashCard()
+    {
+        Front = new FlashCardSide();
+        Back = new FlashCardSide();
+    }
+}
+
+public class FlashCardSide
+{
+    public Layouts Layout { get; set; }
+    public bool ShowBulletPointsIndividually { get; set; }
+}
